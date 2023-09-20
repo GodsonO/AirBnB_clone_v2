f09407d0f70ebfa64e894e89f58f6e9a3d3c2bf9
GodsonO <godsonomoghene15@gmail.com>
2023-09-20 13:58:18 -0700

    airbnb

--- models/__init__.py
+++ models/__init__.py
@@ -6,9 +6,9 @@ initialize the models package
 from os import getenv
 
 
-storage_t = getenv("HBNB_TYPE_STORAGE")
+storage_hbnb = getenv("HBNB_TYPE_STORAGE")
 
-if storage_t == "db":
+if storage_hbnb == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
